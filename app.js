6289c3645718cd1f708c50aa5f28bb079bf8a9f6
Rahul Kadukar <rahulkadukar@yahoo.com>
2017-02-14 21:46:58 -0500

    Trying things

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+//Routes
+app.use(require('./routes'));  
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
 })
